17f04b3ecb9cf67a7cd0614a4e62238393449122
Conley Carter <conleyscomputergreek@outlook.com>
2025-03-10 18:30:26 -0500

    Initial commit

---
+++ src/main/java/com/winver94/randomfeatures/RandomFeaturesClient.java
@@ -0,0 +1,10 @@
+package com.winver94.randomfeatures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomFeaturesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/winver94/randomfeatures/RandomFeaturesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.winver94.randomfeatures;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RandomFeaturesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
